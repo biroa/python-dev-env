5ada7db6fdd5b997aa4058a83165694aa3c559fb
Adam Biro <adam.biro@gmail.com>
2019-06-25 18:21:19 +0200

    add new model definition

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharFields(max_length=200)
+    pub_date= models.DateTimeField('date_published')
+
+class Chioce(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharFields(max_length=200)
+    votes = models.IntegerFields(defaul=0)
