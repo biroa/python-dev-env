0820e85b58c08e603785bb2c7298c22bc8645789
Adam Biro <adam.biro@gmail.com>
2019-06-26 19:35:37 +0200

    set project settings, init model, generate migration and apply migration

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Question(models.Model):
-    question_text = models.CharFields(max_length=200)
+    question_text = models.CharField(max_length = 200)
     pub_date= models.DateTimeField('date_published')
 
 class Chioce(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharFields(max_length=200)
-    votes = models.IntegerFields(defaul=0)
+    choice_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
