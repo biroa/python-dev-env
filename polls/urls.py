b15329f7d0fffdd1dc639454ef306756b9d981ac
Adam Biro <adam.biro@gmail.com>
2019-07-03 18:02:22 +0200

    add new view methods and templates

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'
+# Generic views are the recommended way to use urls
 urlpatterns =[
     # /polls/
     path('', views.index,name='index'),
@@ -11,4 +12,13 @@ urlpatterns =[
     path('<int:question_id>/results/', views.results,name='results'),
     # /polls/5/vote/
     path('<int:question_id>/vote/', views.vote,name='vote'),
+]
+
+# Using urlpatterns as Generic Views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, Http404
-from django.template import loader
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
+
+# In that case if we like to use loader instead of render
+#from django.template import loader
 
 def index(request):
     # Order pud_date -> but pub date in reverse order -pud_date
@@ -26,14 +29,29 @@ def detail(request, question_id):
     # return render(request, 'polls/detail.html', {'question': question})
     #
     # the get_object_or_404 provide the same output as the code above
-    
+
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-      return HttpResponse("You're voting on question %s." % question_id)
+      # We try to get the object from the server with try/catch shortcut
+      question = get_object_or_404(Question, pk=question_id)
+      try:
+          # request.POST is a dictiopnary
+          selected_choice = question.choice_set.get(pk=request.POST['choice']);
+      # raise KeyError when the key is not in the dictionary
+      except(KeyError, Choice.DoesNotExist):
+          # Redisplay questions detail choice
+          return render(request, 'polls/detail.html',{'question':question,
+            'error_message':"You did not select a choice",
+          })
+      else:
+          selected_choice.votes += 1
+          selected_choice.save()
+
+          return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
